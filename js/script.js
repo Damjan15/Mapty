c63ebec8ef584b66c0148957fbad92321886e64a
Damjan15 <stamenovdamjan20@gmail.com>
2022-07-09 10:47:32 -0700

    feature: displaying leaflet map

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
 
-
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 // Geolocation
-
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
 
-        console.log(latitude, longitude)
+      console.log(latitude, longitude);
 
-    }, () => {
-        console.log("Error getting position")
-    }) 
+      // Leaflet
+      const coords = [latitude, longitude]
+
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    () => {
+      alert("Error getting position! Please try again.")
+    }
+  );
 }
